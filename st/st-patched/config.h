84c6a722c4127c00e697cffa8c71868bd8b048b5
Brian Hamilton <zaksab37@example.com>
2019-08-31 21:21:04 -0400

    Updating st/suckless config.

--- st/st-patched/config.h
+++ st/st-patched/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char font[] = "mono:pixelsize=14:autohint=true";
+static char font[] = "inconsolata-g:pixelsize=20:autohint=true";
 //static char font[] = "mono:pixelsize=14:antialias=true:autohint=true";
 static int borderpx = 2;
 
